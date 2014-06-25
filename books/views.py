6ffb7fcb235bae69d86a7023a97b871892d05151
dmitry.krasilov <dmitry.krasilov@C0005.soni.redsolution.ru>
2014-06-25 13:43:47 +0600

    Добавлен шаблон для обработки метаинформации

--- books/views.py
+++ books/views.py
@@ -9,7 +9,9 @@ from django.template import RequestContext
 def display_meta(request):
     values = request.META.items()
     values.sort()
-    html = []
-    for k, v in values:
-        html.append('<tr><td>%s</td><td>%s</td></tr>' % (k, v))
-    return HttpResponse('<table>%s</table>' % '\n'.join(html))
+    return render_to_response('page.html',
+    	{
+    	'values': values
+    	},
+    	RequestContext(request)
+    )
